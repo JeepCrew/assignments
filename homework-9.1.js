9093c9c59099b19470dbddfb09591d6167030826
JeepCrew <ucfnerd02@hotmail.com>
2014-03-28 22:03:03 -0400

    Fourth refactoring.  Simplified multiplying by 0

--- homework-9.1.js
+++ homework-9.1.js
@@ -2,27 +2,22 @@ module.exports = {
 
     multiply: function multiply (a, b) {
  
-        if ( a == -1 ) {
+        if ( a == -1 && b !== +0 ) {
         
             if ( b == -1 ) return +1;
-         
-            if ( b == +0 ) return +0;
              
             return -1;
         }
         
-        if ( a == +0 ) {
-             
-            return -0;
+        if ( a == +0 || b  == +0 ) {
             
+            return +0;
         }
-        
-        if ( a == +1 ) {
+     
+        if ( a == +1 && b !== +0 ) {
              
             if ( b == -1 ) return -1;
-             
-            if ( b == +0 ) return +0;
-             
+            
             return +1;
         }
         
