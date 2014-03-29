b1f607ff035ba570cf974683a21db3fcb4fd396a
JeepCrew <ucfnerd02@hotmail.com>
2014-03-28 21:37:30 -0400

    Created baseline file from class file

---
+++ homework-9.1_test.js
@@ -0,0 +1,114 @@
+var assert = require('assert'),
+    calculator = require('./homework-9.1.js');
+
+/**
+ * Test plan for addition():
+ * 
+ *  1st | 2nd | output
+ * -----+-----+-------
+ *   -1 |  -1 |  -2
+ *   -1 |  +0 |  -1
+ *   -1 |  +1 |  +0
+ *   +0 |  -1 |  -1
+ *   +0 |  +0 |  +0
+ *   +0 |  +1 |  +1
+ *   +1 |  -1 |  +0
+ *   +1 |  +0 |  +1
+ *   +1 |  +1 |  +2
+ */
+
+/**
+ * Test plan for subtraction():
+ * 
+ *  1st | 2nd | output
+ * -----+-----+-------
+ *   -1 |  -1 |  +0
+ *   -1 |  +0 |  -1
+ *   -1 |  +1 |  -2
+ *   +0 |  -1 |  +1
+ *   +0 |  +0 |  +0
+ *   +0 |  +1 |  -1
+ *   +1 |  -1 |  +2
+ *   +1 |  +0 |  +1
+ *   +1 |  +1 |  +0
+ */
+
+/**
+ * Test plan for multiplication():
+ * 
+ * 1st / 2nd / output
+ * -----+-----+------
+ *   -1 |  -1 |  +1
+ *   -1 |  +0 |  +0
+ *   -1 |  +1 |  -1
+ *   +0 |  -1 |  +0
+ *   +0 |  +0 |  +0
+ *   +0 |  +1 |  -0
+ *   +1 |  -1 |  -1
+ *   +1 |  +0 |  +0
+ *   +1 |  +1 |  +1
+ */
+ 
+
+var tests = [
+    [ -1, -1, +1 ],
+    [ -1, +0, +0 ],
+    [ -1, +1, -1 ],
+    [ +0, -1, +0 ],
+    [ +0, +0, +0 ],
+    [ +0, +1, +0 ],
+    [ +1, -1, -1 ],
+    [ +1, +0, +0 ],
+    [ +1, +1, +1 ],
+];
+
+tests.forEach(function(row){
+    var a = row[0],
+        b = row[1],
+        c = row[2];
+        
+    assertEqual(calculator.multiply(a, b), c);
+});
+
+function assertEqual(actual, expected, message){
+    assert.equal(actual, expected, message);
+    console.log('pass!');
+}
+
+
+ /**
+ * Test plan for division():
+ * 
+ * 1st / 2nd / output
+ * -----+-----+------
+ *   -1 |  -1 |  +1
+ *   -1 |  +0 |  +NaN
+ *   -1 |  +1 |  -1
+ *   +0 |  -1 |  +0
+ *   +0 |  +0 |  +NaN
+ *   +0 |  +1 |  +0
+ *   +1 |  -1 |  -1
+ *   +1 |  +0 |  +NaN
+ *   +1 |  +1 |  +1
+ */
+ 
+var tests = [
+    [-1, -1, +1],
+    [-1, +0, null],
+    [-1, +1, -1],
+    [+0, -1, +0],
+    [+0, +0, null],
+    [+0, +1, +0],
+    [+1, -1, -1],
+    [+1, +0, null],
+    [+1, +1, +1],
+/* // Not running...
+// */
+];
+
+tests.forEach(function(row){
+    var a = row[0],
+        b = row[1],
+        c = row[2];
+    assertEqual(calculator.divide(a, b), c, "fail on " + row);
+});
