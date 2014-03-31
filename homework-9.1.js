412afb7fa23ba7dd151c5b6312f183a73f9f2dc7
JeepCrew <ucfnerd02@hotmail.com>
2014-03-31 18:05:04 -0400

    First subtraction refactoring

--- homework-9.1.js
+++ homework-9.1.js
@@ -107,23 +107,35 @@ module.exports = {
     
     subtraction: function subtration (a, b) {
         
-        if ( a == -1 && b == -1 ) return +0;
+        if ( a == -1 ) {
+            
+            if ( b == -1 ) return +0;
         
-        if ( a == -1 && b == +0 ) return -1;
+            if ( b == +0 ) return -1;
         
-        if ( a == -1 && b == +1 ) return -2;
+            return -2;
         
-        if ( a == +0 && b == -1 ) return +1;
+        }
         
-        if ( a == +0 && b == +0 ) return +0;
+        if ( a == +0 ) {
+            
+            if ( b == -1 ) return +1;
         
-        if ( a == +0 && b == +1 ) return -1;
+            if ( b == +0 ) return +0;
         
-        if ( a == +1 && b == -1 ) return +2;
+            return -1;
         
-        if ( a == +1 && b == -1 ) return +1;
+        }
         
-        return +0;
+        if ( a == +1 ) {
+            
+            if ( b == -1 ) return +2;
+        
+            if ( b == -1 ) return +1;
+        
+            return +0;
+        
+        }
         
     }
     
