9c4a677bd72e19fbd71a4aab75d1bc5c7c725d74
JeepCrew <ucfnerd02@hotmail.com>
2014-03-31 18:00:46 -0400

    Added subtraction functions

--- homework-9.1.js
+++ homework-9.1.js
@@ -86,5 +86,45 @@ module.exports = {
             return +2;
         
         }
+    },
+    
+    
+/**
+ * Test plan for subtraction():
+ * 
+ *  1st | 2nd | output
+ * -----+-----+-------
+ *   -1 |  -1 |  +0
+ *   -1 |  +0 |  -1
+ *   -1 |  +1 |  -2
+ *   +0 |  -1 |  +1
+ *   +0 |  +0 |  +0
+ *   +0 |  +1 |  -1
+ *   +1 |  -1 |  +2
+ *   +1 |  +0 |  +1
+ *   +1 |  +1 |  +0
+ */
+    
+    subtraction: function subtration (a, b) {
+        
+        if ( a == -1 && b == -1 ) return +0;
+        
+        if ( a == -1 && b == +0 ) return -1;
+        
+        if ( a == -1 && b == +1 ) return -2;
+        
+        if ( a == +0 && b == -1 ) return +1;
+        
+        if ( a == +0 && b == +0 ) return +0;
+        
+        if ( a == +0 && b == +1 ) return -1;
+        
+        if ( a == +1 && b == -1 ) return +2;
+        
+        if ( a == +1 && b == -1 ) return +1;
+        
+        return +0;
+        
     }
+    
 };
--- homework-9.1_test.js
+++ homework-9.1_test.js
@@ -147,3 +147,38 @@ tests.forEach(function(row) {
         c = row[2];
     assertEqual(calculator.addition(a, b), c, "fail on " + row);
 });
+
+/**
+ * Test plan for subtraction():
+ * 
+ *  1st | 2nd | output
+ * -----+-----+-------
+ *   -1 |  -1 |  +0
+ *   -1 |  +0 |  -1
+ *   -1 |  +1 |  -2
+ *   +0 |  -1 |  +1
+ *   +0 |  +0 |  +0
+ *   +0 |  +1 |  -1
+ *   +1 |  -1 |  +2
+ *   +1 |  +0 |  +1
+ *   +1 |  +1 |  +0
+ */
+ 
+var tests = [
+    [-1, -1, +0],
+    [-1, +0, -1],
+    [-1, +1, +0],
+    [+0, -1, +1],
+    [+0, +0, +0],
+    [+0, +1, -1],
+    [+1, -1, +2],
+    [+1, +0, +1],
+    [+1, +1, +0],
+];
+
+tests.forEach(function(row) {
+    var a = row[0],
+        b = row[1],
+        c = row[2];
+    assertEqual(calculator.addition(a, b), c, "fail on " + row);
+});
