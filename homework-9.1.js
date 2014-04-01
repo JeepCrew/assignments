d6b2f096c5103482911e714e9b6773b8b6f9d0a9
JeepCrew <ucfnerd02@hotmail.com>
2014-04-01 14:53:42 -0400

    Refactored division, added cases for a or b equaling +0

--- homework-9.1.js
+++ homework-9.1.js
@@ -1,8 +1,6 @@
 module.exports = {
 
     multiply: function multiply (a, b) {
- 
-       
         
         if ( a == +0 || b  == +0 ) {
             
@@ -21,14 +19,28 @@ module.exports = {
 
     divide: function divide (a, b) {
         
-        if ( a == -1 && b !== +0 ) {
+        if ( b == +0 ) {
             
-            if ( b == -1 ) return +1;
-            
-            return -1;
+            return null;
             
         }
         
+        if ( a == +0 ) {
+            
+            return +0;
+            
+        }
+        
+        if ( ( a == -1 || b == -1 ) && (a == +1 || b == +1 ) ) {
+            
+            return -1;
+        
+        }
+        
+        return +1;
+            
+        
+        /**
         if ( a == +0 && b !== +0) {
             
             if ( b == -1 ) return +0;
@@ -42,13 +54,7 @@ module.exports = {
             
             return +1;
         }
-        
-        if ( b == +0 ) {
-            
-            return null;
-            
-        }
-    
+        */
     },
     
     addition: function addition (a, b) {
@@ -83,23 +89,6 @@ module.exports = {
         
         }
     },
-    
-    
-/**
- * Test plan for subtraction():
- * 
- *  1st | 2nd | output
- * -----+-----+-------
- *   -1 |  -1 |  +0
- *   -1 |  +0 |  -1
- *   -1 |  +1 |  -2
- *   +0 |  -1 |  +1
- *   +0 |  +0 |  +0
- *   +0 |  +1 |  -1
- *   +1 |  -1 |  +2
- *   +1 |  +0 |  +1
- *   +1 |  +1 |  +0
- */
 
     subtraction: function subtration (a, b) {
         
